ec47f6a1da487cb70ff017b2b66a60b2e4f24619
jasinetemi0220 <jasinetemi0220@gmail.com>
2025-01-18 16:05:32 +0100

    HEllo
    Routing

--- e-comm/src/App.js
+++ e-comm/src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
-
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/hompage/hompage.component";
 
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <HomePage />
-      </div>
-    );
-  }
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
